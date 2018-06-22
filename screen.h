eef74649044d8cb413c8ef526dfbedbe32e6f6cb
SirIsaacNeutron <dbadir@cox.net>
2018-06-21 17:29:10 -0700

    Fixed Screen::display to work on smaller files
    Previously Screen::display would cause Vimperor to crash when opening
    smaller files because Screen::display did not check to see if it had
    reached the end of the file yet. The "iterator" variable in its for-loop
    would keep getting incremented, and would access memory it was not
    supposed to access.
    
    Now Screen::display takes 2 iterators so that it can know when it has
    displayed the entire file.
    
    I changed the Editor object in main.cpp to open the makefile for this
    project instead of editor.cpp to test the new Screen::display (and to
    prepare to test additional features, like printing ~'s in the way vim
    does)

--- screen.h
+++ screen.h
@@ -15,9 +15,10 @@ struct Screen {
 	Screen(const Screen& s) = delete;
 	Screen& operator=(const Screen& s) = delete;
 
-	void display(std::vector<std::string>::iterator begin) const noexcept;
+	void display(std::vector<std::string>::iterator begin,
+			std::vector<std::string>::iterator end) const noexcept;
 	void show_first_display(std::vector<std::string>::iterator 
-			begin) const noexcept;
+			begin, std::vector<std::string>::iterator end) const noexcept;
 
 	void move_cursor(const Cursor& cursor) const noexcept;
 	void scroll_down(std::vector<std::string>::iterator begin) noexcept;
