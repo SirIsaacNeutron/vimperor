a66502bd02c01bf4816a5b8a162f601a266bb8fa
SirIsaacNeutron <dbadir@cox.net>
2018-06-26 11:09:02 -0700

    Vimperor can now take command-line arguments
    Currently Vimperor only runs if we provide one argument to it (the name
    of the file we want to edit).

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "editor.h"
 
-int main() {
-	Editor e{"editor.cpp"};	
+void run_editor(const char* file_name) {
+	Editor e{file_name};	
 
 	int character;
 	while (true) {
 		character = getch();
 		e.process_keypress(character);
 	}
-	return 0;
+}
+
+int main(int argc, char** argv) {
+	if (argc > 0) {
+		run_editor(argv[1]);
+	}
 }
