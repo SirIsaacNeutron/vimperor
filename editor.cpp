3a649c5b5bf35e425f675171073adcf9b4d2f72c
SirIsaacNeutron <dbadir@cox.net>
2018-07-19 13:26:16 -0700

    Fixed a bug in delete_char()
    See commit 3db1ed2 to see what bug was fixed.

--- editor.cpp
+++ editor.cpp
@@ -128,20 +128,22 @@ void Editor::insert_mode_action(int character) noexcept {
 }
 
 void Editor::delete_char() noexcept {
-	// Always deleting here means that Vimperor can display the deletion
-	// of the first character in a line correctly
-	file_contents[file_contents_index].replace(cursor.x, 1, "");
+	// If we're not past the end of the file
+	if (file_contents_index < file_contents.size()) {
+		file_contents[file_contents_index].replace(cursor.x, 1, "");
 
-	// If a line is empty and we just pressed the delete key at the left-
-	// most edge of the screen
-	if (cursor.x == 0 
-			&& file_contents[file_contents_index].size() == 0) {
-		// Then delete the whole line	
-		file_contents.erase(std::begin(file_contents) + file_contents_index);
+		// If a line is empty and we just pressed the delete key at the left-
+		// most edge of the screen
+		if (cursor.x == 0 
+				&& file_contents[file_contents_index].size() == 0) {
+			// Then delete the whole line	
+			file_contents.erase(std::begin(file_contents) + file_contents_index);
+		}
+		else {
+			move_cursor_left();	
+		}
 	}
-	else {
-		move_cursor_left();	
-	}
+
 	screen.display(std::begin(file_contents) + top_of_screen_index,
 			std::end(file_contents));
 }
